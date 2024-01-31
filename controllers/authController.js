1ec9826adcb7ae7d647174e6b2d20a48b6feb45c
sawthetphyoe <sawthetphyoe28498@gmail.com>
2024-01-31 19:55:00 +0630

    update cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,10 +17,9 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    domain:"http://68.183.179.232:3000",
-    secure: false,
-    sameSite: 'None',
+    httpOnly: false,
+    // secure: true,
+    // sameSite: 'None',
   };
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = false;
 
